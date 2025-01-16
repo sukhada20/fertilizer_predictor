6bfcfd7d545a2336c4195fc03a11353ba53f4e54
Sukhada Bhoyar <sukhadabhoyar@gmail.com>
2025-01-16 18:43:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ Original file is located at
 from google.colab import drive
 drive.mount('/content/drive')
 
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
